d01a6658880b00dddac0d3b263439b6e46d2699a
Developer <dev@example.com>
2026-02-07 20:34:09 +0530

    backend cors added

--- backend/index.ts
+++ backend/index.ts
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://smart-api-monitoring-platform.onrender.com"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/endpoints', endpointRoutes);
